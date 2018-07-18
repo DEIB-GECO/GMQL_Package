2e0553a39c7095883f3ac258c2d856766eef09c8
akaitoua <abdulrahman.kaitoua@polimi.it>
2018-07-18 11:48:56 +0200

    running script with GMQLV2.1

--- examples/runScriptExamples.sh
+++ examples/runScriptExamples.sh
@@ -8,28 +8,19 @@ echo $SCRIPT_DIR
 
 cd $(dirname $0)
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0  LOCAL|MAPREDUCE " >&2
-  echo "The datasets should first be added to GMQL repository, running createInputDataSets.sh script"
+#if [ "$#" -ne 1 ]; then
+  echo "Usage: $0" >&2
+  echo "WARN: The datasets should be first added to GMQL repository, then run createInputDataSets.sh script"
   echo "Example: ./runScriptExamples.sh"
-  exit 1
-fi
+#  exit 1
+#fi
 
 Script1="../bin/GMQL-Submit-R -scriptpath $SCRIPT_DIR/scripts/script1.GMQL"
-Script2="GMQLScriptManager compilerun $1 $SCRIPT_DIR/scripts/script2.GMQL"
-Script3="GMQLScriptManager compilerun $1  $SCRIPT_DIR/scripts/script3.GMQL"
-Script4="GMQLScriptManager compilerun $1 $SCRIPT_DIR/scripts/script4.GMQL"
-showall="repositoryManagerV1 list all"
+Script2="../bin/GMQL-Submit-R -scriptpath $SCRIPT_DIR/scripts/script2.GMQL"
+Script3="../bin/GMQL-Submit-R -scriptpath $SCRIPT_DIR/scripts/script3.GMQL"
+Script4="../bin/GMQL-Submit-R -scriptpath $SCRIPT_DIR/scripts/script4.GMQL"
+showall="repositoryManager list all"
 
-echo "-------------------------------------------"
-echo "-------------------------------------------"
-echo "-------------------------------------------"
-echo "Register Username: $USER"
-echo "Executing Command: $registeruser"
-echo "-------------------------------------------"
-echo "-------------------------------------------"
-echo "-------------------------------------------"
-$registeruser
 echo "-------------------------------------------"
 echo "-------------------------------------------"
 echo "-------------------------------------------"
