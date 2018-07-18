134631261e4da5f167f6bf16be441a60b8da6ff7
akaitoua <abdulrahman.kaitoua@polimi.it>
2018-07-18 11:48:22 +0200

    Installation with disregards to GMQL version

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ mvn install
 
 # Copy output dependencies to execution folder
 cp ./GMQL-Cli/target/GMQL-Cli-*-jar-with-dependencies.jar ../lib/
-cp ./GMQL-Repository/target/uber-GMQL-Repository-*.jar ../lib/
+cp ./GMQL-Repository/target/GMQL-Repository-*-jar-with-dependencies.jar ../lib/
 cp ./GMQL-SManager/target/GMQL-SManager-*-jar-with-dependencies.jar ../lib/
